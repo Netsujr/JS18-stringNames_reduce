dff6ca3f8670745f2719d2dbcaa781a5c686f3fb
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-31 17:18:13 +0900

    added hrs,min,secs to Total

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'))
+const total = document.getElementById('total');
 timeNodes.map(node => node.textContent += `: ${node.dataset.time}`);
+console.log(total);
 
 
 const seconds = timeNodes
@@ -11,15 +13,17 @@ const seconds = timeNodes
 
   }).reduce((total, videoSeconds) => total + videoSeconds);
 
-  let secondsLeft = seconds;
-  const hours = Math.floor(secondsLeft / 3600);
-  secondsLeft = secondsLeft % 3600;
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
 
-  const mins = Math.floor(secondsLeft / 60);
-  secondsLeft = secondsLeft % 60;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
 
 
 // console.log(mins);
-  console.log(hours, mins, secondsLeft);
-  console.log(seconds);
-  // console.log(secondsLeft);
+console.log(hours, mins, secondsLeft);
+console.log(seconds);
+// console.log(secondsLeft);
+
+total.textContent += `${hours}:${mins}:${secondsLeft}`;
