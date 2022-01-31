c047f6f698251fa0585a17cd1745dd7fa7fb1133
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-31 17:06:35 +0900

    got an array of seconds
    added time to each video in html

---
+++ app.js
@@ -0,0 +1,15 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+timeNodes.map(node => node.textContent += `: ${node.dataset.time}`);
+
+
+const seconds = timeNodes
+.map(node => node.dataset.time)
+.map(timeCode => {
+ const [mins, secs] = timeCode.split(':').map(parseFloat);
+ return (mins * 60) + secs;
+ console.log(mins, secs);
+})
+;
+
+
+console.log(seconds);
