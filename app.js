6b1fc9a9f70f87af1877a14dfaec9e6654b0bfc4
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-31 17:08:44 +0900

    returning total number of seconds

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ timeNodes.map(node => node.textContent += `: ${node.dataset.time}`);
 
 
 const seconds = timeNodes
-.map(node => node.dataset.time)
-.map(timeCode => {
- const [mins, secs] = timeCode.split(':').map(parseFloat);
- return (mins * 60) + secs;
- console.log(mins, secs);
-})
-;
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins * 60) + secs;
+    console.log(mins, secs);
+
+  }).reduce((total, videoSeconds) => total + videoSeconds)
+  ;
 
 
 console.log(seconds);
